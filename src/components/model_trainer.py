4839628789200525a4fc8f9da6e5160b74467e3a
imkenova <imk@gmail.com>
2024-05-03 21:43:34 +0300

    edit model trainer

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -11,7 +11,9 @@ import warnings
 
 warnings.filterwarnings('ignore')
 
-mlflow.set_tracking_uri('https://dagshub.com/imkenova/Credit_Card_Default_Prediction--main.mlflow')
+mlflow.set_tracking_uri('https://dagshub.com/imkenova/project_fraud_detection.mlflow')
+os.environ['MLFLOW_TRACKING_USERNAME'] = 'imkenova'
+os.environ['MLFLOW_TRACKING_PASSWORD'] = '2a258ef043924179df455e1024a246bfed159099'
 
 
 @dataclass(frozen=True)
